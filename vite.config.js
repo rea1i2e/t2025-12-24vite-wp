b8aa71b6e0949f5250f28f9551c5af566945ae6e
Yoshiaki Numajiri <afmaar128@gmail.com>
2026-03-14 14:10:53 +0900

    png圧縮変更

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ import { fileURLToPath } from "node:url";
 import imageSize from "image-size";
 import viteImagemin from "@vheemstra/vite-plugin-imagemin";
 import imageminMozjpeg from "imagemin-mozjpeg";
-import imageminPngquant from "imagemin-pngquant";
+import imageminOptipng from "imagemin-optipng";
 import imageminGifsicle from "imagemin-gifsicle";
 import imageminSvgo from "imagemin-svgo";
 import imageminWebp from "imagemin-webp";
@@ -287,10 +287,12 @@ export default defineConfig({
       root: path.resolve(__dirname), // 絶対パスを維持（相対パスNG）
       onlyAssets: true,
       include: /\.(png|jpe?g|gif|svg)$/i,
+      exclude: [/node_modules/],
+      // exclude: [/node_modules/, /mv_bottom_bg\.png$/i], // mv_bottom_bg.pngは最適化しない場合は除外
       plugins: {
         // 静的インポートに変更
         jpg: imageminMozjpeg({ quality: envNumber("VITE_JPEG_QUALITY", 75), progressive: true }),
-        png: imageminPngquant({ quality: [0.65, 0.8], speed: 3 }),
+        png: imageminOptipng({ optimizationLevel: 2 }),
         gif: imageminGifsicle({ optimizationLevel: 2 }),
         svg: imageminSvgo()
       },
